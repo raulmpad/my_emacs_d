514362611ae7335c3f57a008e51c2859b922b5f4
Raul Martinez Padilla <raul@deceroainfinito.es>
2012-10-08 04:27:03 +0200

    more plugins and best config

--- raul.el
+++ raul.el
@@ -30,7 +30,30 @@
 ;; Rinari
 (add-to-list 'load-path "~/.emacs.d/rinari")
 (require 'rinari)
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (defadvice ruby-mode-set-encoding
+              (around ruby-mode-set-encoding-disable activate) nil)))
 
+;;autocomplete
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
+;; Rsense
+(setq rsense-home "/Users/raul/.dotfiles/emacs/plugins/rsense-0.3")
+(add-to-list 'load-path (concat rsense-home "/etc"))
+(require 'rsense)
+ 
+;; Rsense + Autocomplete
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-rsense-method)
+            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
+;; Ruby-electric
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
 
 ;; yasnippets
 (add-to-list 'load-path
@@ -54,3 +77,6 @@
 ;; font size
 (set-face-attribute 'default nil :height 160)
 
+;;Slime-style navigation of Emacs Lisp source with M-. & M-,
+(require 'elisp-slime-nav)
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
